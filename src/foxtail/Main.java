d9aceace7568a6b5b770951f2d1be9288f0f0ffc
Julius Behrens <juliusbehrens@gmx.com>
2018-03-02 17:01:38 +0100

    Implemented standard board creation and toString methods

--- src/foxtail/Main.java
+++ src/foxtail/Main.java
@@ -1,12 +1,18 @@
 package foxtail;
 
 import foxtail.board.Board;
+import foxtail.board.BoardBuilder;
+import foxtail.piece.Pawn;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(Board.getBooleanRow(7).size());
+        BoardBuilder boardBuilder = new BoardBuilder();
+        boardBuilder.setPiece(new Pawn(Color.BLACK,1));
+        Board board = boardBuilder.build();
+        System.out.println(board.toString());
     }
 
 }
